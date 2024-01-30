176ec3889684f74daafc8f7ea0ca15304fc6e638
Nikita Kirikov <n.kiirkov@gmail.com>
2024-01-30 21:34:26 +0300

    add task1 random list

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,4 +1,40 @@
 package task1;
 
+import java.util.ArrayList;
+import java.util.OptionalDouble;
+
+/*
+Создать коллекцию класса ArrayList наполнить ее рандомными элементами типа Integer
+С помощью stream:
+- Удалить думбликаты
+- Вывести все четные элементы в диапазоне от 7 до 17 (включительно)
+- Каждый элемлемент умножить на 2
+- Отсортировать и вывести на экран первые 4 элемента
+- Вывести количество элементов в стриме
+- Вывести среднее арифметическое всех чисел в stream
+ */
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 1; i < 20; i++) {
+            arr.add((int) (Math.random() * 100));
+        }
+
+        arr.stream().distinct().filter(i -> i > 7 && i <= 17)
+                .forEach(System.out::println);
+
+        System.out.println("\n");
+
+        arr.stream().map(i -> i * 2)
+                .sorted()
+                .limit(4).forEach(System.out::println);
+        System.out.println("\n");
+
+        int a = (int) arr.stream().count();
+        System.out.println(a);
+
+        OptionalDouble average = arr.stream().mapToInt(x -> x).average();
+        System.out.println(average);
+    }
 }
